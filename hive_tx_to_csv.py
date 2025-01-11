76452edb66d933c1c92e4f3dae63914e51adb822
pharesim <pharesim@protonmail.com>
2025-01-11 16:37:03 +0100

    exclude claim_account ops

--- hive_tx_to_csv.py
+++ hive_tx_to_csv.py
@@ -6,9 +6,9 @@ import requests
 import json
 
 # Set parameters
-account_names = ['account1','account2']
-start_date = datetime(2023, 12, 1)
-end_date = datetime(2024, 1, 1)
+account_names = ['pharesim']
+start_date = datetime(2024, 9, 1)
+end_date = datetime(2024, 10, 1)
 
 # Initialize the Hive blockchain instance
 hive = Hive(node=['https://api.hive.blog','https://api.deathwing.me'])
@@ -44,7 +44,7 @@ def get_transactions_for_account(account_name, start_date, end_date):
 
    # Operations to include/exclude (not working with history_reverse, needs to be filtered in loop)
     ops = ['return_vesting_delegation','delegate_vesting_shares','transfer_to_vesting','proposal_pay','fill_order','author_reward','comment_benefactor_reward','convert','fill_convert_request','producer_reward','curation_reward','fill_vesting_withdraw','transfer']
-    excluded = ['proxy_cleared','create_claimed_account','account_created','witness_update','account_update','witness_set_properties','vote','effective_comment_vote','account_witness_vote','comment','claim_reward_balance','update_proposal_votes','custom_json','comment_reward','comment_payout_update','comment_options','withdraw_vesting','delayed_voting','limit_order_create','limit_order_cancelled']
+    excluded = ['claim_account','proxy_cleared','create_claimed_account','account_created','witness_update','account_update','witness_set_properties','vote','effective_comment_vote','account_witness_vote','comment','claim_reward_balance','update_proposal_votes','custom_json','comment_reward','comment_payout_update','comment_options','withdraw_vesting','delayed_voting','limit_order_create','limit_order_cancelled']
        
     # Iterate over account history
     scanned_tx = 0
